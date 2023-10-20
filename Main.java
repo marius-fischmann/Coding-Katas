6f69d04119a09cd798ed28590f345ed1086af7c2
Marius-Fischmann <marius.fischmann1@gmail.com>
2023-10-20 15:06:43 +0200

    check pushing to different branches from different IDEs

--- Main.java
+++ Main.java
@@ -2,5 +2,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("hello github world");
+        System.out.println("Is 42 dividable by 2? " + isDividableBy2(42));
+    }
+
+    private static boolean isDividableBy2(int number){
+        return number%2==0;
     }
 }

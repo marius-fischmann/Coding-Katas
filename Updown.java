16156f22e34fb27558128c1d022b93f0181e8927
act943 <marius.fischmann@haw-hamburg.de>
2023-10-22 19:02:30 +0200

    switching from array-based implementation to arraylist based implementation

--- Updown.java
+++ Updown.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 
 /**
@@ -7,52 +8,42 @@ public class Updown {
 
     public static String arrange(String strng) {
         // 1. split the strings
-        String[] strings = strng.split(" ");
+        ArrayList<String> strings = new java.util.ArrayList<>(Arrays.stream(strng.split(" ")).toList());
 
         // 2. swap the strings according to the property:
         // P = length t(O) <= length t(1) >= length t(2) <= length t(3) >= length t(4) .... (P)
-        for (int i = 0; i < strings.length-1; i++) {
+        for (int i = 0; i < strings.size()-1; i++) {
             if (!stringMatchesTheProperty(i, strings)) {
-                swapString(i, strings);
+                String tempString = strings.get(i);
+                strings.set(i, strings.get(i+1));
+                strings.set(i+1, tempString);
             }
         }
 
         // 3. change the smaller substrings to lower and the other ones to upper case
-        for (int i = 0; i < strings.length; i++) {
+        for (int i = 0; i < strings.size(); i++) {
             if (i%2==0) {
-                strings[i] = strings[i].toLowerCase();
+                strings.set(i, strings.get(i).toLowerCase());
             }
             else{
-                strings[i] = strings[i].toUpperCase();
+                strings.set(i, strings.get(i).toUpperCase());
             }
         }
 
         // 4. return the strings as one string
-        return Arrays.stream(strings).filter(e1 -> !e1.equals(strings[0])).reduce(strings[0], (s, s2) -> s + " " + s2);
+        return strings.subList(1, strings.size()).stream().reduce(strings.get(0), (s, s2) -> s + " " + s2);
     }
 
-    private static boolean stringMatchesTheProperty(int index, String[] strings){
+    private static boolean stringMatchesTheProperty(int index, ArrayList<String> strings){
+        // P = length t(O) <= length t(1) >= length t(2) <= length t(3) >= length t(4) .... (P)
         // check if string s needs to be and is already smaller than the following string
-        if(index%2==0 && strings[index].length() <= strings[index+1].length()){
+        if(index%2==0 && strings.get(index).length() <= strings.get(index+1).length()){
             return true;
         }
         // check if string s needs to be and is already greater than the following string
-        if(index%2==1 && strings[index].length() >= strings[index+1].length()){
+        if(index%2==1 && strings.get(index).length() >= strings.get(index+1).length()){
             return true;
         }
         return false;
-    }
-
-    /**
-     * swaps the string in the given index of the String[] with the one of at index+1
-     * @param index
-     * @param strings
-     * @return the given String[] with swaps strings at index and index+1
-     */
-    private static String[] swapString(int index, String[] strings){
-        String tempString = strings[index];
-        strings[index] = strings[index+1];
-        strings[index+1] = tempString;
-        return strings;
     }
 }

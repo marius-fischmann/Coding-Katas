173074fd4a906d9787a4edc9bf9b56a2380b469c
marius-fischmann <marius.fischmann1@gmail.com>
2023-10-22 19:20:38 +0200

    check git credentials

--- Main.java
+++ Main.java
@@ -1,8 +1,11 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello world");
-        System.out.println("hello github world");
-        System.out.println("Is 42 dividable by 2? " + isDividableBy2(42));
     }
 
     private static boolean isDividableBy2(int number){
